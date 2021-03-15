c733ad25425edef1029fc80002831aba3e2946d0
Perry <mesagp2@sp21-cs423-49.cs.illinois.edu>
2021-03-15 18:35:42 -0500

    fop write arg handling

--- mp2/mp2.c
+++ mp2/mp2.c
@@ -11,7 +11,8 @@
 #define FILENAME "status"
 #define DIRECTORY "mp2"
 #define RW_PERMISSION 0666                      // allows read, write but not execute
-#define LONG_BUFF_SIZE 21                       // number of digits in ULLONG_MAX + 1
+#define BUFF_SIZE 128
+#define DECIMAL_BASE 10
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("mesagp2");
@@ -36,35 +37,98 @@ static struct proc_dir_entry *proc_dir;
 static struct proc_dir_entry *proc_entry;
 
 /* mp2_read - outputs a list of processes and their scheduling parameters */
-static ssize_t mp2_read ( struct file *file, char __user *buffer,
+static ssize_t mp2_read( struct file *file, char __user *buffer,
 									size_t count, loff_t *data ) {
 	return 0;
 }
 
+/* get_next_arg - places next arg into buffer and returns size */
+static size_t get_next_arg(char *buff, char *arg_buff, loff_t *pos) {
+	size_t arg_size;
+
+	/* skip past whitespace and commas to argument */
+	while (buff[*pos] == ' ' || buff[*pos] == ',') {
+		(*pos)++;
+	}
+
+	/* extract arg */
+	for ( 	arg_size = 0;
+		  	buff[*pos] != '\0' && buff[*pos] != '\n' && buff[*pos] != ',';
+		  	(*pos)++, arg_size++ ) {
+		arg_buff[arg_size] = buff[*pos];
+	}
+
+	/* null terminate arg buffer */
+	arg_buff[arg_size++] = '\0';
+
+	return arg_size;
+}
+
 /* mp2_write - interface for userapps to register, yield, or de-register */
-static ssize_t mp2_write ( struct file *file, const char __user *buffer,
+static ssize_t mp2_write( struct file *file, const char __user *buffer,
 									size_t count, loff_t *data ) {
-	char procfs_buffer[LONG_BUFF_SIZE];
-	size_t procfs_buffer_size;
-	ssize_t res;
+	char procfs_buff[BUFF_SIZE];
+	ssize_t procfs_size;
+	char arg_buff[BUFF_SIZE];
+	loff_t pos;
+	char operation;
+	pid_t pid;
+    unsigned long period;
+    unsigned long processing_time;
+	int error;
 
 	/* copy buffer into kernel space */
-	procfs_buffer_size = count;
-	if (procfs_buffer_size > LONG_BUFF_SIZE) {
-		procfs_buffer_size = LONG_BUFF_SIZE;
-	}
-	res = simple_write_to_buffer( procfs_buffer,
-                                  LONG_BUFF_SIZE,
+	procfs_size = simple_write_to_buffer( procfs_buff,
+                                  BUFF_SIZE,
                                   data,
                                   buffer,
-                                  procfs_buffer_size );
-	if (res < 0) {
-		return res;
+                                  count );
+	if (procfs_size < 0) {
+		return procfs_size;
 	}
 
-	switch (procfs_buffer[0]) {
+	/* setup to parse args */
+	pos = 0;
+
+	/* get operation arg */
+	operation = procfs_buff[pos++];
+
+	/* get PID arg */
+	get_next_arg(procfs_buff, arg_buff, &pos);
+	error = kstrtol(arg_buff, DECIMAL_BASE, (long *) &pid);
+	if (error) {
+		return error;
+	}
+
+	switch (operation) {
 		case 'R':
 			printk(KERN_ALERT "write case R");
+
+			/* get period arg */
+			get_next_arg(procfs_buff, arg_buff, &pos);
+			error = kstrtol(arg_buff, DECIMAL_BASE, &period);
+			if (error) {
+				return error;
+			}
+
+			/* get processing time arg */
+			get_next_arg(procfs_buff, arg_buff, &pos);
+			error = kstrtol(arg_buff, DECIMAL_BASE, &processing_time);
+			if (error) {
+				return error;
+			}
+
+            // struct mp2_task_struct *pcb;
+
+            // /* get the userapp's task_struct */
+            // find_task_by_pid();
+
+            // /* allocate cache for PCB */
+            // pcb = (struct mp2_task_struct*) kmalloc(sizeof(struct mp2_task_struct), GFP_KERNEL);
+
+            // /* init task to SLEEPING state */
+            // pcb->state = SLEEPING;
+
 			break;
 
 		case 'Y':
@@ -76,10 +140,7 @@ static ssize_t mp2_write ( struct file *file, const char __user *buffer,
 			break;
 	}
 
-	/* fix string to terminate */
-	procfs_buffer[procfs_buffer_size] = '\0';
-
-	return res;
+	return procfs_size;
 }
 
 /* mp2_fops - stores links read and write functions to mp2 file */
